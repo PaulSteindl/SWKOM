373f0f5ae6a5523bb9d3614ca035798bd19baa41
Battlecoob <if20b031@technikum-wien.at>
2022-11-17 13:00:36 +0100

    changed CreateMap<>() for more complex mappings (hop-warehouse, parcel)

--- out/src/PowerPuffBoys.SWKOM.Package.Services/Helpers/AutomapperProfiles.cs
+++ out/src/PowerPuffBoys.SWKOM.Package.Services/Helpers/AutomapperProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+
 using Business = PowerPuffBoys.SWKOM.BusinessLogic.Entities; // source
 using DTO = PowerPuffBoys.SWKOM.Package.Services.DTOs; // dtos
 
@@ -12,14 +13,28 @@ namespace PowerPuffBoys.SWKOM.Package.Services.Helpers
 
             CreateMap<Business.Error, DTO.Error>().ReverseMap();
             CreateMap<Business.GeoCoordinate, DTO.GeoCoordinate>().ReverseMap();
-            CreateMap<Business.Hop, DTO.Hop>().ReverseMap();
             CreateMap<Business.HopArrival, DTO.HopArrival>().ReverseMap();
-            // CreateMap<Business.Parcel, DTO.Parcel>(); // komplexer wegen mult. dto -> 1 business
             CreateMap<Business.Recipient, DTO.Recipient>().ReverseMap();
             CreateMap<Business.Transferwarehouse, DTO.Transferwarehouse>().ReverseMap();
             CreateMap<Business.Truck, DTO.Truck>().ReverseMap();
+            CreateMap<Business.Hop, DTO.Hop>()
+                .Include<Business.Warehouse, DTO.Warehouse>().ReverseMap();
             CreateMap<Business.Warehouse, DTO.Warehouse>().ReverseMap();
             CreateMap<Business.WarehouseNextHops, DTO.WarehouseNextHops>().ReverseMap();
+
+            /* more complex entity mappings */
+            // parcel
+            CreateMap<Business.Parcel, DTO.Parcel>()
+                .ForMember(dest => dest.Recipient, opt => opt.MapFrom(src => src.Recipient))
+                .ForMember(dest => dest.Sender, opt => opt.MapFrom(src => src.Sender))
+                .ForMember(dest => dest.Weight, opt => opt.MapFrom(src => src.Weight));
+            CreateMap<Business.Parcel, DTO.NewParcelInfo>()
+                .ForMember(dest => dest.TrackingId, opt => opt.MapFrom(src => src.TrackingId));
+            CreateMap<Business.Parcel, DTO.TrackingInformation>()
+                .ForMember(dest => dest.State, opt => opt.MapFrom(src => src.State))
+                .ForMember(dest => dest.VisitedHops, opt => opt.MapFrom(src => src.VisitedHops))
+                .ForMember(dest => dest.FutureHops, opt => opt.MapFrom(src => src.FutureHops));
+
         }
     }
 }
